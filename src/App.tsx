254decdf72275cfb9b931c7239f4fc523dfa8994
zjx <2023654801@qq.com>
2024-10-21 00:10:45 +0800

    feat: 导入各种包

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
-
+import "./output.css"
 function App() {
   return (
     <div className="App">
       这是曦景的博客
+      <div className='w-[100%] text-center'>hellow</div>
     </div>
   );
 }
